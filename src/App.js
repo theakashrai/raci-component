4f16d49c7299ef7c90374ab5c352e6b41b94fa53
Akash Kumar Rai <EVOLVUS\akashk@ELPBLR-073.evolvus.com>
2019-09-24 00:10:33 +0530

    second one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Matrix from './Matrix';
 
 function App() {
+  let cols = ['FSG Council'	,'Executive Committee Chair','Committee Chair',	'Advisory Group Members'	,'Business Leads'	,'Business Stewards Ops',	'AS Ops',	'Project Manager'];
+  let rows = ['Develop strategy & communicate vision','Oversee Financial System Governance body','Define & monitor performance metrics (KPIs)','Schedule and conduct FSG group meetings','Establish priorities for ongoing support, maintenance, critical issues and enhancement of the application','Manage scope, resources and issues related to system operations and enhancements','Cross-functional coordination of ongoing operational concerns, patches, fixes, upgrades, and enhancements','Provide progress on performance metrics (KPIs)','Provide status and progress of ongoing FMS iniitiative (non-BA initiatives) projects','Identify opportunities for process and system improvements, evaluate options, propose enhancements','Consolidate list of proposed enhancements','Define and document business requirements for system enhancements','Provide technical estimates and plan for enhancement requests','Develop charter for all enhancements for each cycle','Review and approve enhancement charter','Approve enhancement charter budget'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Matrix row={rows} col={cols}/>
+      
     </div>
   );
 }
